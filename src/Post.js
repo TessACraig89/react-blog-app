28a8c64e7f0f33b83a3a5720bffb0a45a8f19132
Tess Craig <tessashleycraig89@gmail.com>
2018-01-29 15:42:41 -0600

    modifies Post's render method so that body is equal to data from state instead of props

--- src/Post.js
+++ src/Post.js
@@ -18,21 +18,28 @@ class Post extends Component {
       comments: ["My first comments at first", "My second comment at first", "My third comment at first"]
     }
   }
+  editBlogPost (e) {
+    this.setState({
+      body: prompt("update blog post", this.state.body)
+    })
+  }
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <p>By {this.props.author}</p>
-        <div>
-          <p>{this.props.body}</p>
+      <div className="Post">
+        <header className="Post-header">
+          <h1>{this.props.title}</h1>
+          <p>By {this.props.author}</p>
+        </header>
+        <article className="Post-body">
+          <h3>Comments:</h3>
+          {/* use map to loop through comments array and for each comment run function that  */}
+              {/* returns Comment body (from Comment.js) with value of comment (from index.js)  */}
+          {this.state.comments.map((comment) => {
+              return <Comment body={comment} />
+            })}
+          </article>
+          <button onClick={(e) => this.editBlogPost(e)}>Update Post?</button>
         </div>
-        <h3>Comments:</h3>
-        {/* use map to loop through comments array and for each comment run function that  */}
-            {/* returns Comment body (from Comment.js) with value of comment (from index.js)  */}
-        {this.state.comments.map((comment) => {
-            return <Comment body={comment} />
-          })}
-      </div>
     );
   }
 }
